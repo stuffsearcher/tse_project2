dd501edcded0dccb5af078189921c259fb7345c5
stuffsearcher <explorer473@gmail.com>
2024-11-18 10:35:47 +0000

    Bueeeeno

--- temp.py
+++ temp.py
@@ -1,9 +1,19 @@
 import pandas
+import matplotlib.pyplot as plt
 
-f = pandas.read_csv('cwd\data2.csv')
+f = pandas.read_csv("cwd/data.csv")
 
-y = pandas.cut(f['Speed'],[0,2,4,6,8,10,12,14])
- 
-f['bins'] = y
+f.columns = ["Date", "Speed", "Direction"]
 
-f = f.set_index('Time') #f.set_index('Time', inplace=True)
+y = pandas.cut(f["Speed"], [0, 2, 4, 6, 8, 10, 12, 14])
+
+f["bins"] = y
+
+# f = f.set_index("Date")  or f.set_index('Time', inplace=True)
+f["Date"] = pandas.to_datetime(f["Date"])
+filt = f["Date"].dt.hour == 4
+f.set_index("Date")["Speed"].resample("D").mean().count()
+f.groupby("bins").mean()["Speed"]
+plt.hlines(f["Speed"].mean(), 0, len(f))
+plt.plot(range(len(f)), f["Speed"])
+plt.show()
