61a8812cf022d322383a9a437318ae2471413fe1
stuffsearcher <explorer473@gmail.com>
2024-11-20 20:59:08 +0000

    Plot with all power curves

--- power_curves.py
+++ power_curves.py
@@ -1,7 +1,15 @@
 import preparation
 
+import matplotlib.pyplot as plt
+
 turbine_data = preparation.prepare_turbine_data(data_name="turbine_data")
 
 enercon_data = turbine_data[turbine_data["Turbine"] == "Enercon E-53"]
 gamesa_data = turbine_data[turbine_data["Turbine"] == "Gamesa G97 (2000kW)"]
 vestas_data = turbine_data[turbine_data["Turbine"] == "Vestas V-117 (4.2MW)"]
+
+plt.plot(enercon_data["Wind [m/s]"], enercon_data["Power P [kW]"])
+plt.plot(gamesa_data["Wind [m/s]"], gamesa_data["Power P [kW]"])
+plt.plot(vestas_data["Wind [m/s]"], vestas_data["Power P [kW]"])
+
+plt.show()
