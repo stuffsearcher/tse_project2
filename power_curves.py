c81c7e4135de0274afca51f7688c2a419ca70295
stuffsearcher <explorer473@gmail.com>
2024-11-20 20:54:35 +0000

    Separate data for each turbine

--- power_curves.py
+++ power_curves.py
@@ -1,3 +1,7 @@
 import preparation
 
 turbine_data = preparation.prepare_turbine_data(data_name="turbine_data")
+
+enercon_data = turbine_data[turbine_data["Turbine"] == "Enercon E-53"]
+gamesa_data = turbine_data[turbine_data["Turbine"] == "Gamesa G97 (2000kW)"]
+vestas_data = turbine_data[turbine_data["Turbine"] == "Vestas V-117 (4.2MW)"]
