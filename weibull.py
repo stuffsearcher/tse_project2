6bc8241f380c636758f24a30fed08abd4de5b25d
stuffsearcher <explorer473@gmail.com>
2024-11-27 18:28:13 +0000

    Weibull at 20m

--- weibull.py
+++ weibull.py
@@ -46,22 +46,37 @@ shape, loc, scale = weibull_min.fit(data_df["speed"], loc=-1)
 x = np.linspace(data_df["speed"].min(), data_df["speed"].max(), len(data_df["speed"]))
 pdf = weibull_min.pdf(x, beta, loc, alpha)
 plt.plot(x, pdf * 100, "r--", label="Fitted Weibull PDF")
-plt.bar(bar_x_axis, bar_data.values * 100, edgecolor="black")
-plt.title("prueba")
-plt.xticks(rotation=90)
-plt.show()
-
-
-plt.hist(
-    data_df["speed"],
-    bins=speed_bins,
-    density=True,
-    alpha=0.6,
-    color="skyblue",
-    label="Distribuion",
+plt.bar(
+    bar_x_axis,
+    bar_data.values * 100,
     edgecolor="black",
+    color="skyblue",
+    label="Distribution",
+    alpha=0.6,
 )
-plt.plot(x, pdf, "r--", label="Fitted Weibull PDF")
+plt.title("prueba")
+plt.ylabel("Frequency [%]")
+plt.xlabel("Wind Speed [m/s]")
+plt.title("Weibull Distribution for Wind Speed at 20 meters")
+plt.xticks(rotation=90)
 plt.legend()
-plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
 plt.show()
+
+# plt.plot(x, pdf, "r--", label="Fitted Weibull PDF")
+
+# plt.xticks(rotation=90)
+# plt.hist(
+#     data_df["speed"],
+#     bins=speed_bins,
+#     density=True,
+#     alpha=0.6,
+#     color="skyblue",
+#     label="Distribuion",
+#     edgecolor="black",
+# )
+# plt.legend()
+# plt.ylabel("Frequency [%]")
+# plt.xlabel("Wind Speed [m/s]")
+# plt.title("Weibull Distribution for Wind Speed at 20 meters")
+# plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+# plt.show()
