b99e2f6557106fce433f904eb0c1c043fe8bd36c
stuffsearcher <explorer473@gmail.com>
2024-11-28 11:17:16 +0000

    Annual yield calculation

---
+++ annual_yield.py
@@ -0,0 +1,38 @@
+import preparation
+
+import numpy as np
+import pandas as pd
+
+data_df = preparation.prepare_main_data(data_name="data", annual=True)
+turbine_data = preparation.prepare_turbine_data(data_name="turbine_data")
+
+
+def calculate_annual_yield(wind_series: pd.core.series.Series, turbine_name: str):
+    filt = turbine_data["Turbine"] == turbine_name
+    turbine_height = turbine_data[filt]["Height [m]"].iloc[0]
+
+    wnd_series_at_x_height = wind_series.apply(
+        preparation.log_law, args=(turbine_height,)
+    )
+
+    power_curve = turbine_data[filt]["Power P [kW]"]
+
+    wind_speeds = np.arange(1, 31)  # Wind speeds from 1 to 30 m/s
+
+    power_output = np.interp(wnd_series_at_x_height, wind_speeds, power_curve)
+
+    time_interval = 1 / 6  # Conversion from 10-minute intervals
+    total_energy = np.sum(power_output) * time_interval
+
+    max_power = power_curve.max()
+    theoretical_maximum = max_power * 8760
+    capicity_factor = (total_energy / theoretical_maximum) * 100
+
+    print(
+        f"Total energy yield/capacity factor for {turbine_name} in one year: {total_energy/1e6:.2f} GWh / {capicity_factor:.2f}  "
+    )
+    return total_energy
+
+
+for tb_name in turbine_data["Turbine"].unique():
+    calculate_annual_yield(turbine_name=tb_name, wind_series=data_df["speed"])
--- preparation.py
+++ preparation.py
@@ -1,13 +1,18 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy.special import gamma
 
 
-def prepare_main_data(data_name: str):
+def prepare_main_data(data_name: str, annual=False):
     data_df = pd.read_csv(f"cwd/{data_name}.csv")
 
     # Name and type handling
     data_df.columns = ["date", "speed", "wnd_direction"]
     data_df["date"] = pd.to_datetime(data_df["date"])
+    data_df.dropna(inplace=True)
+
+    if annual:
+        data_df = prepare_for_annual(data_df)
 
     # Speed and direction data binning
     max_speed = int(np.round(data_df["speed"].max()))
@@ -33,6 +38,63 @@ def log_law(vel: np.float64, turbine_height: float):
         np.log(turbine_height / open_flat_roughness)
         / np.log(data_height / open_flat_roughness)
     )
+
+
+def prepare_for_annual(dataframe):
+    mean = dataframe["speed"].mean()
+
+    variations = {
+        "summer": {
+            "mean": mean,
+            "start_date": "2016-06-01",
+            "end_date": "2016-08-31",
+        },
+        "autumn": {
+            "mean": mean * 1.05,
+            "start_date": "2016-09-01",
+            "end_date": "2016-11-30",
+        },
+        "winter": {
+            "mean": mean * 1.1,
+            "start_date": "2016-12-01",
+            "end_date": "2017-02-28",
+        },
+        "spring": {
+            "mean": mean * 1.05,
+            "start_date": "2016-03-01",
+            "end_date": "2016-05-31",
+        },
+    }
+
+    new_df = pd.DataFrame({"date": [], "speed": [], "wnd_direction": []})
+
+    for season in variations.items():
+        datetime_series = pd.date_range(
+            start=season[1]["start_date"], end=season[1]["end_date"], freq="10min"
+        )
+        if season[0] == "summer":
+            # Define the date range to exclude
+            start_drop = pd.Timestamp("2016-07-01")
+            end_drop = pd.Timestamp("2016-08-01")
+
+            # Filter the datetime_series to exclude the range
+            datetime_series = datetime_series[
+                (datetime_series < start_drop) | (datetime_series >= end_drop)
+            ]
+
+        desired_mean = season[1]["mean"]
+
+        shape = 2.125117825030581
+        scale = desired_mean / gamma(1 + 1 / shape)
+
+        size = len(datetime_series)
+
+        weibull_series = scale * np.random.weibull(shape, size)
+
+        temp_df = pd.DataFrame({"date": datetime_series, "speed": weibull_series})
+        new_df = pd.concat([new_df, temp_df], ignore_index=True)
+
+    return new_df
 
 
 if __name__ == "__main__":
